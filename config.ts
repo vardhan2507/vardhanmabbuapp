28f635cfd09e796c646f240d2d89ac1c9d9139b2
vardhan250798 <vardhan@inncircles.com>
2020-06-05 14:06:32 +0530

    changed default port to 4000

--- config.ts
+++ config.ts
@@ -1,18 +1,7 @@
 class Config {
 
     //general
-    private readonly port = process.env.PORT || 3000;
-    private readonly socketPort = process.env.socketPort || 4444;
-    //security
-    private readonly FIREBASE_CONFIG = {
-        apiKey: process.env.firebase_apikey || null,
-        authDomain: process.env.firebase_authDomain || null,
-        databaseURL: process.env.firebase_databaseURL || null,
-        projectId: process.env.firebase_projectId || null,
-        storageBucket: process.env.firebase_storageBucket || null,
-        messagingSenderId: process.env.firebase_messagingSenderId || null,
-        appId: process.env.firebase_appId || null
-    }
+    private readonly port = process.env.PORT || 4000;
 
     public get config(): any {
         return this;
